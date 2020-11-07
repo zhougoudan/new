d9e3049a4886c1a9db317c5a93f70e4fcccf0873
ZhouLiang <2324445771@qq.com>
2020-11-07 23:41:25 +0800

    commit and push

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("驱动初始化成功");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
