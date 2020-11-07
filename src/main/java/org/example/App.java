410924e49b1ac771f3b458f8cf13484337ec45c2
ZhouLiang <2324445771@qq.com>
2020-11-07 23:52:52 +0800

    test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ public class App
     {
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("驱动初1始功");
+            System.out.println("驱动1始功");
         }
         catch(Exception e){
             e.printStackTrace();
